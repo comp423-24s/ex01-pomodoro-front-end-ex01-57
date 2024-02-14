26f95379d1625177af215ce894bed0090ea2b35f
mayamcp5 <mayamcp@icloud.com>
2024-02-13 21:43:02 -0500

    Story A Subtask 1 Completion

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -38,6 +38,7 @@ import { AboutComponent } from './about/about.component';
 import { GateComponent } from './gate/gate.component';
 import { ProfileEditorComponent } from './profile/profile-editor/profile-editor.component';
 import { EventFilterPipe } from './event/event-filter/event-filter.pipe';
+import { ProductivityComponent } from './productivity/productivity.component';
 
 @NgModule({
   declarations: [
@@ -47,7 +48,8 @@ import { EventFilterPipe } from './event/event-filter/event-filter.pipe';
     HomeComponent,
     AboutComponent,
     GateComponent,
-    ProfileEditorComponent
+    ProfileEditorComponent,
+    ProductivityComponent
   ],
   imports: [
     /* Angular */
